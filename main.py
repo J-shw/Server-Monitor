7572f79206611b730044620c5235a71f636ebfd7
Josh <91017208+J-shw@users.noreply.github.com>
2024-09-05 22:37:10 +0100

    Removed extra import

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.exc import IntegrityError
 from flask_login import UserMixin, LoginManager, login_user, logout_user, login_required, current_user
 from flask_bcrypt import Bcrypt
-from flask_login import LoginManager
 from flask_apscheduler import APScheduler
 from icmplib import ping
 from waitress import serve
